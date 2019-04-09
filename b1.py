62a8d44268a2a07824950d9464c7beaf34c9b120
Joseph Pu <josephpu@hotmail.com>
2019-04-08 23:06:23 -0400

    Bongo test code

---
+++ b1.py
@@ -0,0 +1,18 @@
+
+a={
+	"key1":1,
+	"key2":{
+		"key3":1,
+		"key4":{
+			"key5":4
+		}
+	}
+}
+
+def print_depth(d, level=1):
+	for (k,v) in d.items():
+		print(k, level)
+		if isinstance(v,dict):
+			print_depth(v,level+1)
+
+print_depth(a)
